d022737b406bfe112fcd5e8df1275686dee89f12
cintia1522 <cintiamchavez@gmail.com>
2022-05-26 17:19:16 -0300

    avanzando con mercado liebre

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
-const express= require("express");
-const path= require("path");
-const app= express();
+const express = require("express");
+const path = require("path");
+const app = express();
 
 app.use(express.static(path.join(__dirname, "public")));
 
 // llamando_servidor
 app.listen(8080, () => {
-    console.log("Servidor corriendo!!!💻");
-})
-// Rutas
+        console.log("Servidor corriendo!!!💻");
+    })
+    // Rutas
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "./views/home.html"));
+        res.sendFile(path.join(__dirname, "./views/home.html"));
+    })
+    //Register
+app.get("/register", (req, res) => {
+        res.sendFile(path.join(__dirname, "./views/register.html"));
+    })
+    //Login
+app.get("/login", (req, res) => {
+    res.sendFile(path.join(__dirname, "./views/login.html"));
 })
